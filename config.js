ee5aaf0ec9baf6ad68303021f5e2cf3454c3d6ec
tulonkhan <125132046+tulonkhan@users.noreply.github.com>
2026-04-06 18:09:26 +0300

    Update owner information and bot token in config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  ownerUsernames: ["MOHAMMADNAYAN"],//Your Username
-  ownerNumber: "+8801615298449",//Your Number
+  ownerUsernames: ["TxTulon"],//Your Username
+  ownerNumber: "+8801741955183",//Your Number
   autoUpdate: true,
-  admin: ["5920596290"], // your id
+  admin: ["1935714816"], // your id
   prefix: "/", // dont change prefix 
-  telegramBotToken: "your bot token",//Your Bot Tokens
+  telegramBotToken: "6613940836:AAHlaKB4pPKEUFREAC_I-TcJQgaGtfeYVb8",//Your Bot Tokens
   imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
   port: process.env.PORT || 8053 || 8053, // port
   mess: {
-    ownerlink: "https://t.me/MOHAMMADNAYAN",
+    ownerlink: "https://t.me/NXT_Trader",
     eror: "Internal Server Eror 😵",
     owner: "Sorry, this command can only be accessed by the owner!",
     group: "Sorry, this command can only be used within a group!",
